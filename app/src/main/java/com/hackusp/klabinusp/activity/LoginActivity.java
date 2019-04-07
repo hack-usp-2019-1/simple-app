fca12fab4174eb595f395559699069c1ffa0301f
Alexadre Batistella Bellas <alexandre.bellas@usp.br>
2019-04-07 04:01:30 -0300

    Mais funcionalidades implementadas

--- app/src/main/java/com/hackusp/klabinusp/activity/LoginActivity.java
+++ app/src/main/java/com/hackusp/klabinusp/activity/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,7 +19,16 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_login);
+        this.getSupportActionBar().hide();
+
 
         textoNumeroUSP = findViewById(R.id.textoNumeroUSP);
         textoSenha = findViewById(R.id.textoSenha);
--- app/src/main/java/com/hackusp/klabinusp/activity/OportunidadeActivity.java
+++ app/src/main/java/com/hackusp/klabinusp/activity/OportunidadeActivity.java
@@ -2,6 +2,8 @@ package com.hackusp.klabinusp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hackusp.klabinusp.R;
 import com.hackusp.klabinusp.model.Oportunidade;
@@ -9,11 +11,25 @@ import com.hackusp.klabinusp.model.Oportunidade;
 public class OportunidadeActivity extends AppCompatActivity {
 
     private Oportunidade oportunidade;
+    private TextView textTituloOportunidade, textDescricaoOportunidade, textLocalOportunidade,
+            textAssuntoOportunidade, textTipoOportunidade, textLinkOportunidade,
+            textDataOportunidade, textDistanciaOportunidade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oportunidade);
+
+        textTituloOportunidade = findViewById(R.id.textTituloOportunidade);
+        textDescricaoOportunidade = findViewById(R.id.textDescricaoOportunidade);
+        textLocalOportunidade = findViewById(R.id.textLocalOportunidade);
+        textAssuntoOportunidade = findViewById(R.id.textAssuntoOportunidade);
+        textTipoOportunidade = findViewById(R.id.textTipoOportunidade);
+        textLinkOportunidade = findViewById(R.id.textLinkOportunidade);
+        textDataOportunidade = findViewById(R.id.textDataOportunidade);
+        textDistanciaOportunidade = findViewById(R.id.textDistanciaOportunidade);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Recuperar dados do usuário destinatário
         Bundle bundle = getIntent().getExtras();
@@ -22,12 +38,20 @@ public class OportunidadeActivity extends AppCompatActivity {
             if(bundle.containsKey("oportunidade")){
 
                 oportunidade = (Oportunidade) bundle.getSerializable("oportunidade");
-
-
+                textTituloOportunidade.setText(oportunidade.getTitulo());
+                textDescricaoOportunidade.setText(oportunidade.getDescricao());
+                textLocalOportunidade.setText(oportunidade.getLocal());
+                textAssuntoOportunidade.setText(oportunidade.getAssunto());
+                textTipoOportunidade.setText(oportunidade.getTipo());
+                textLinkOportunidade.setText(oportunidade.getLink());
+                textDataOportunidade.setText(oportunidade.getData());
+                textDistanciaOportunidade.setText(oportunidade.getDistancia());
 
             }
             else{
 
+                Toast.makeText(this, "Erro ao enviar a oportunidade. Tente novamente.", Toast.LENGTH_SHORT).show();
+                finish();
 
             }
         }
